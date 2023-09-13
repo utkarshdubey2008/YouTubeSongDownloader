8ddc255a68325fa409a064aed6878dc33a1dac21
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2023-09-13 09:56:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     API_ID = "7721764"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "a9c08aae19aa4c8b37ff658d1951a1f7" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "2025346520:AAFVr3FNtz59hxdm_TCLNDmw1x3rMNHCIiw" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    BOT_TOKEN = "6500050793:AAGkVJ7pWf2heX3h0Wfde7SVINgtEdsPPP8" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
